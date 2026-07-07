1e6c72885a1b5ba50e778de718a1c3d4d62bc61f
mariyamnavila <bibimariyamnavila@gmail.com>
2026-07-07 16:25:38 +0600

    implement authentication features: add login and refresh token functionality, update interfaces, and create JWT utility

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -17,6 +17,56 @@ const registerUser = catchAsync(async (req: Request, res: Response, next: NextFu
     })
 })
 
+const loginUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
+
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24
+    })
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 * 7
+
+    })
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "User logged in successfully",
+        data: { accessToken, refreshToken }
+    });
+})
+
+const refreshToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    const { accessToken } = await authService.refreshToken(refreshToken);
+
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 // 24 hour or 1 day
+    })
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Token Refreshed Successfully",
+        data: {
+            accessToken,
+        },
+    })
+})
+
 export const authController = {
     registerUser,
+    loginUser,
+    refreshToken,
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
-import { IRegisterUser } from "./auth.interface"
+import { ILoginUser, IRegisterUser } from "./auth.interface"
 import config from "../../config";
 import { UserRole } from "../../../generated/prisma/enums";
+import { jwtUtils } from "../../utils/jwt";
+import { JwtPayload, SignOptions } from "jsonwebtoken";
 
 const registerUserIntoDB = async (payload: IRegisterUser) => {
 
@@ -54,6 +56,84 @@ const registerUserIntoDB = async (payload: IRegisterUser) => {
     return user;
 }
 
+const loginUser = async (payload: ILoginUser) => {
+    const { email, password } = payload;
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            email,
+        },
+    });
+
+    if (user.status === "BANNED") {
+        throw new Error("Your account has been banned. Please contact support to reactive.")
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+        throw new Error("Password is incorrect");
+    }
+
+    const jwtPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+    };
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_access_secret,
+        config.jwt_access_expires_in as SignOptions,
+    )
+
+    const refreshToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_refresh_secret,
+        config.jwt_refresh_expires_in as SignOptions,
+    );
+
+    return { accessToken, refreshToken };
+}
+
+const refreshToken = async (refreshToken: string) => {
+    const verifiedRefreshToken = jwtUtils.verifyToken(refreshToken, config.jwt_refresh_secret);
+
+    if (!verifiedRefreshToken.success) {
+        throw new Error(verifiedRefreshToken.error)
+    }
+
+    const { id } = verifiedRefreshToken.data as JwtPayload;
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            id,
+        },
+    })
+
+    if (user.status === "BANNED") {
+        throw new Error("User is blocked")
+    }
+
+    const jwtPayload = {
+        id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+    }
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_access_secret,
+        config.jwt_access_expires_in as SignOptions,
+    )
+
+    return { accessToken };
+}
+
 export const authService = {
     registerUserIntoDB,
+    loginUser,
+    refreshToken,
 }
